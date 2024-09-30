11b6df2467e6f4f61792d4d1b02f6cc60f1af510
Jerinkc <jerinkc22@gmail.com>
2024-09-30 16:02:02 +0530

    Remove people/_topbar and index.html.slim Root route change to people_controller and associated changes to show action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,19 +1,17 @@
 class PeopleController < ApplicationController
-  def index
-    user_query = UserQuery.new(current_user)
-    @friends = user_query.people_with_transactions
-  end
-
   def show
     user_query = UserQuery.new(current_user)
     @friends = user_query.people_with_transactions.order(created_at: :desc)
-    @person = @friends.find_by(id: params[:id])
-
-    return unless @person.present?
-
+    @person = @friends.find_by(id: selected_person_id)
     @transactions_summary = TransactionsSummaryPresenter.new(current_user, @person)
     @transactions = user_query.transactions_with(@person).map do |t|
                       TransactionPresenter.new(t, current_user, @person)
                     end
+  end
+
+  private
+
+  def selected_person_id
+    params[:id] || current_user.id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :people, only: [:index, :show] do
+  resources :people, only: [:show] do
     resources :settlements, only: [:index, :new, :create]
   end
   resources :expenses, only: [:index, :new, :create]
 
-  root to: "people#index"
+  root to: "people#show"
   get 'dummy/:id', to: 'static#person'
 end
